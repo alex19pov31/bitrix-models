41e4c4f3ee81cd39120a7fbc3e1db47adeee3ba9
Alexander Nesterov <alex19pov31@gmail.com>
2019-07-06 15:20:06 +0300

    move iblock event hanlers to trait

--- src/BaseIblockModel.php
+++ src/BaseIblockModel.php
@@ -6,99 +6,20 @@ use CIBlockResult;
 use Alex19pov31\BitrixModel\Exceptions\ExceptionAddElementIblock;
 use Alex19pov31\BitrixModel\Exceptions\ExceptionUpdateElementIblock;
 use Bitrix\Main\Loader;
-use Bitrix\Main\EventManager;
 use Alex19pov31\BitrixModel\Traits\IblockTrait;
 use Alex19pov31\BitrixModel\Traits\IblockSeoTrait;
 use Alex19pov31\BitrixModel\Traits\IblockFeatureTrait;
+use Alex19pov31\BitrixModel\Traits\IblockEventTrait;
 
 abstract class BaseIblockModel extends BaseModel
 {
     use IblockTrait;
     use IblockSeoTrait;
     use IblockFeatureTrait;
+    use IblockEventTrait;
 
     abstract protected static function getIblockId(): int;
     abstract protected static function getCacheMinutes(): int;
-
-
-    public static function listenEvents()
-    {
-        if (!static::$em) {
-            static::$em = EventManager::getInstance();
-        }
-
-        Loader::includeModule('iblock');
-        static::$em->addEventHandler('iblock', 'OnBeforeIBlockElementAdd', [
-            static::class,
-            'onBeforeCreate',
-        ]);
-        static::$em->addEventHandler('iblock', 'OnAfterIBlockElementAdd', [
-            static::class,
-            'onAfterCreate',
-        ]);
-        static::$em->addEventHandler('iblock', 'OnBeforeIBlockElementUpdate', [
-            static::class,
-            'onBeforeUpdate',
-        ]);
-        static::$em->addEventHandler('iblock', 'OnAfterIBlockElementUpdate', [
-            static::class,
-            'onAfterUpdate',
-        ]);
-        static::$em->addEventHandler('iblock', 'OnAfterIBlockElementDelete', [
-            static::class,
-            'onAfterDelete',
-        ]);
-    }
-
-    final public static function onBeforeCreate(&$arFields)
-    {
-        if (static::getIblockId() == $arFields['IBLOCK_ID']) {
-            static::beforeCreate($arFields);
-        }
-    }
-
-    final public static function onAfterCreate(&$arFields)
-    {
-        if (static::getIblockId() == $arFields['IBLOCK_ID']) {
-            static::afterCreate($arFields);
-        }
-    }
-
-    final public static function onBeforeUpdate(&$arParams)
-    {
-        if (static::getIblockId() == $arParams['IBLOCK_ID']) {
-            static::beforeUpdate($arParams);
-        }
-    }
-
-    final public static function onAfterUpdate(&$arFields)
-    {
-        if (static::getIblockId() == $arFields['IBLOCK_ID']) {
-            static::afterUpdate($arFields);
-        }
-    }
-
-    final public static function onAfterDelete(&$arFields)
-    {
-        if (static::getIblockId() == $arFields['IBLOCK_ID']) {
-            static::afterDelete($arFields);
-        }
-    }
-
-    protected static function beforeCreate(&$arFields)
-    { }
-
-    protected static function afterCreate(&$arFields)
-    { }
-
-    protected static function beforeUpdate(&$arParams)
-    { }
-
-    protected static function afterUpdate(&$arFields)
-    { }
-
-    protected static function afterDelete(&$arFields)
-    { }
 
     public static function getList(array $params = []): CIBlockResult
     {
--- src/BaseIblockModelD7.php
+++ src/BaseIblockModelD7.php
@@ -6,12 +6,14 @@ use Bitrix\Main\ORM\Data\DataManager;
 use Alex19pov31\BitrixModel\Traits\IblockTrait;
 use Alex19pov31\BitrixModel\Traits\IblockSeoTrait;
 use Alex19pov31\BitrixModel\Traits\IblockFeatureTrait;
+use Alex19pov31\BitrixModel\Traits\IblockEventTrait;
 
 abstract class BaseIblockModelD7 extends BaseDataManagerModel
 {
     use IblockTrait;
     use IblockSeoTrait;
     use IblockFeatureTrait;
+    use IblockEventTrait;
 
     abstract protected static function getIblockId(): int;
     abstract protected static function getCacheMinutes(): int;
